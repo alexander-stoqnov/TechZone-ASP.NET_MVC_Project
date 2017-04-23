34df9fa8a2cd5043fbe37b4a183fd311099c0208
Petar Dimitrov <lbmikesmith@gmail.com>
2017-04-23 20:58:14 +0300

    Reduced the ReviewDetails View by creating a display Template for comments and moving the script to a seperate file.

---
+++ TechZone.Web/Scripts/CustomScripts/ReviewVoting.js
@@ -0,0 +1,13 @@
+﻿function castUpVote() {
+    var currentUpVoteCount = $("#useful").text();
+    $("#useful").text(Number(currentUpVoteCount) + 1);
+}
+function castDownVote() {
+    var currentDownVoteCount = $("#useless").text();
+    $("#useless").text(Number(currentDownVoteCount) + 1);
+}
+
+function userAlreadyVoted() {
+    $('#error-message-here').toggle("slow");
+    $('#error-message-here').text("You have already voted for this review!");
+}
