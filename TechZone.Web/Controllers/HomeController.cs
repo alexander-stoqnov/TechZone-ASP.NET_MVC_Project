79d7b566f46d9da12cdf3a0b099de085f7b85d70
Petar Dimitrov <lbmikesmith@gmail.com>
2017-04-26 20:48:40 +0300

    Minor cosmetic changes and optimizations.

--- TechZone.Web/Controllers/HomeController.cs
+++ TechZone.Web/Controllers/HomeController.cs
@@ -23,13 +23,6 @@
             return View(hpvm);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             string path = Server.MapPath("~/Scripts/CustomScripts/");
