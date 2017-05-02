43e87dbbb8312f40171bf27179f37bd212b11247
Petar Dimitrov <lbmikesmith@gmail.com>
2017-05-02 12:24:30 +0300

    Added validation for submitting a review.

--- TechZone.Models/ViewModels/Reviews/SubmitReviewViewModel.cs
+++ TechZone.Models/ViewModels/Reviews/SubmitReviewViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace TechZone.Models.ViewModels.Reviews
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class SubmitReviewViewModel
     {
         public int Id { get; set; }
@@ -7,5 +9,19 @@
         public int ReviewId { get; set; }
 
         public bool AlreadyReviewed { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Please give only a rating between 1 and 5")]
+        public int Rating { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "Title must be at least 10 characters long")]
+        [MaxLength(50, ErrorMessage = "Title cannot be longer than 50 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(100, ErrorMessage = "Please write at least 100 characters.")]
+        [MaxLength(3000, ErrorMessage = "Review cannot be more than 3000 characters long")]
+        public string Content { get; set; }
     }
 }
